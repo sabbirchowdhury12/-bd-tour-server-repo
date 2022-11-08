f9c02a9aaed935b350acf727fca823fa8f81e658
sabbir <sabbir@gmail.com>
2022-11-08 15:34:16 +0600

    get limited data

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
     res.send('hellow world');
 });
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
+
 
 async function run() {
 
     try {
+
+        const Services = client.db('bdTour').collection('services');
+
+        //get data for limit 
+        app.get('/limitServices', async (req, res) => {
+            const cursor = Services.find({});
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        });
 
     }
     finally {
