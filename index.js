17fa0fbb6499334af9867a36bd040a88c4ed482c
sabbir <sabbir@gmail.com>
2022-11-08 18:01:22 +0600

    get data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,14 @@ async function run() {
         app.get('/services', async (req, res) => {
             const cursor = Services.find({});
             const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        //get data bt ID
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await Services.findOne(query);
             res.send(result);
         });
 
